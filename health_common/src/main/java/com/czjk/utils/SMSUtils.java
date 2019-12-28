285adb55c2b11a514b2b27bf0821371206341847
Rainbow Cat <2056423011@qq.com>
2019-12-28 18:39:51 +0800

    阿里云短信工具类

--- health_common/src/main/java/com/czjk/utils/SMSUtils.java
+++ health_common/src/main/java/com/czjk/utils/SMSUtils.java
@@ -1,24 +1,28 @@
 package com.czjk.utils;
 
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
-import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
-import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
-import com.aliyuncs.profile.IClientProfile;
 
 /**
  * @Author: Haotian
- * @Date: 2019/12/27 21:09
+ * @Date: 2019/12/28 16:43
  * @Description: 短信发送工具类
- **/
+ */
 public class SMSUtils {
+
+    private static final String ACCESSKEYID = "LTAI4FvW1pB55RxgiDA4FNJV";
+    private static final String ACCESSKEYSECRET = "e52TSSbx3ZbgUJbU5FUsyUH9z5QgTw";
+
     /**
      * 发送短信验证码
      */
-    public static final String VALIDATE_CODE = "SMS_159620392";
+    public static final String VALIDATE_CODE = "SMS_181491160";
 
     /**
      * 体检预约成功通知
@@ -28,46 +32,31 @@ public class SMSUtils {
     /**
      * 发送短信
      *
-     * @param phoneNumbers
-     * @param param
-     * @throws ClientException
+     * @param templateCode 短信模板ID。请在控制台模板管理页面模板CODE一列查看。
+     * @param phoneNumbers 接收短信的手机号码。国内短信：11位手机号码。
+     * @param authCode     验证码
      */
-    public static void sendShortMessage(String templateCode, String phoneNumbers, String param) throws ClientException {
-        // 设置超时时间-可自行调整
-        System.setProperty( "sun.net.client.defaultConnectTimeout", "10000" );
-        System.setProperty( "sun.net.client.defaultReadTimeout", "10000" );
-        // 初始化ascClient需要的几个参数
-        final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
-        final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
-        // 替换成你的AK
-        final String accessKeyId = "LTAIY8Mawj3VMseR";// 你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "2eHxOORZf4YzpQebROfOoXBFLHJCRY";// 你的accessKeySecret，参考本文档步骤2
-        // 初始化ascClient,暂时不支持多region（请勿修改）
-        IClientProfile profile = DefaultProfile.getProfile( "cn-hangzhou", accessKeyId, accessKeySecret );
-        DefaultProfile.addEndpoint( "cn-hangzhou", "cn-hangzhou", product, domain );
-        IAcsClient acsClient = new DefaultAcsClient( profile );
-        // 组装请求对象
-        SendSmsRequest request = new SendSmsRequest();
-        // 使用post提交
+    public static void sendShortMessage(String templateCode, String phoneNumbers, String authCode) {
+        DefaultProfile profile = DefaultProfile.getProfile( "cn-hangzhou", ACCESSKEYID, ACCESSKEYSECRET );
+        IAcsClient client = new DefaultAcsClient( profile );
+        CommonRequest request = new CommonRequest();
         request.setMethod( MethodType.POST );
-        // 必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
-        request.setPhoneNumbers( phoneNumbers );
-        // 必填:短信签名-可在短信控制台中找到
-        request.setSignName( "传智健康" );
-        // 必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode( templateCode );
-        // 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-        // 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-        request.setTemplateParam( "{\"code\":\"" + param + "\"}" );
-        // 可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
-        // request.setSmsUpExtendCode("90997");
-        // 可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
-        // request.setOutId("yourOutId");
-        // 请求失败这里会抛ClientException异常
-        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse( request );
-        if (sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals( "OK" )) {
-            // 请求成功
-            System.out.println( "请求成功" );
+        request.setDomain( "dysmsapi.aliyuncs.com" );
+        request.setVersion( "2017-05-25" );
+        request.setAction( "SendSms" );
+        request.putQueryParameter( "RegionId", "cn-hangzhou" );
+        request.putQueryParameter( "PhoneNumbers", phoneNumbers );
+        //短信签名名称。请在控制台签名管理页面签名名称一列查看。
+        request.putQueryParameter( "SignName", "传智健康" );
+        request.putQueryParameter( "TemplateCode", templateCode );
+        request.putQueryParameter( "TemplateParam", "{\"code\":\"" + authCode + "\"}" );
+        try {
+            CommonResponse response = client.getCommonResponse( request );
+            System.out.println( response.getData() );
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
         }
     }
 }
