0bb4435f5d37de97ca799c44608b46f544bf2dbb
Rainbow Cat <2056423011@qq.com>
2019-12-29 21:23:16 +0800

    freemarker静态化编码

--- health_service_provider/src/main/java/com/czjk/service/impl/SetmealServiceImpl.java
+++ health_service_provider/src/main/java/com/czjk/service/impl/SetmealServiceImpl.java
@@ -1,5 +1,6 @@
 package com.czjk.service.impl;
 
+import cn.hutool.core.io.file.FileWriter;
 import cn.hutool.core.util.ArrayUtil;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.czjk.constant.RedisConstant;
@@ -10,13 +11,20 @@ import com.czjk.pojo.Setmeal;
 import com.czjk.service.SetmealService;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import redis.clients.jedis.JedisPool;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @Author: Haotian
@@ -30,6 +38,10 @@ public class SetmealServiceImpl implements SetmealService {
     private SetmealDao setmealDao;
     @Autowired
     private JedisPool jedisPool;
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+    @Value("${out_put_path}")
+    private String outPutPath;  //从属性文件中读取要生成的html对应的目录
 
     @Override
     public void add(Setmeal setmeal, Integer[] checkGroupIds) {
@@ -38,6 +50,76 @@ public class SetmealServiceImpl implements SetmealService {
         this.setSetmealAndCheckGroup( setmeal.getId(), checkGroupIds );
         //将图片名称保存到Redis
         jedisPool.getResource().sadd( RedisConstant.SETMEAL_PIC_DB_RESOURCES, setmeal.getImg() );
+        //当添加套餐后需要重新生成静态页面（套餐列表页面、套餐详情页面）
+        generateMobileStaticHtml();
+    }
+
+    /**
+     * 生成当前添加方法所需的静态页面
+     */
+    private void generateMobileStaticHtml() {
+        //准备模板文件中所需的数据
+        List<Setmeal> list = setmealDao.findAll();
+        //生成套餐列表静态页面
+        generateMobileSetmealListHtml( list );
+        //生成套餐详情静态页面（多个）
+        generateMobileSetmealDetailHtml( list );
+    }
+
+    /**
+     * 生成套餐列表静态页面
+     *
+     * @param list 页面数据
+     */
+    private void generateMobileSetmealListHtml(List<Setmeal> list) {
+        Map<String, Object> dataMap = new HashMap<>();
+        //为模板提供数据，用于生成静态页面
+        dataMap.put( "setmealList", list );
+        generateHtml( "mobile_setmeal.ftl", "m_setmeal.html", dataMap );
+    }
+
+    /**
+     * 生成套餐详情静态页面
+     *
+     * @param list 页面数据
+     */
+    private void generateMobileSetmealDetailHtml(List<Setmeal> list) {
+        for (Setmeal setmeal : list) {
+            Map<String, Object> dataMap = new HashMap<>();
+            //为模板提供对应id所有数据
+            dataMap.put( "setmeal",setmealDao.findById( setmeal.getId() ) );
+            generateHtml( "mobile_setmeal_detail.ftl", "setmeal_detail_" + setmeal.getId() + ".html", dataMap );
+        }
+    }
+
+    /**
+     * 通用的方法，用于生成静态页面
+     *
+     * @param templateName freeMarker模板名
+     * @param htmlPageName 生成后的页面名
+     * @param dateMap      页面所需数据
+     */
+    private void generateHtml(String templateName, String htmlPageName, Map<String, Object> dateMap) {
+        //获得配置对象
+        Configuration configuration = freeMarkerConfigurer.getConfiguration();
+        BufferedWriter writer = null;
+        try {
+            //选定生成模板
+            Template template = configuration.getTemplate( templateName );
+            //构造输出流
+            writer = new FileWriter( outPutPath + htmlPageName, "UTF-8" ).getWriter( false );
+            //输出文件
+            template.process( dateMap, writer );
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //流不为空进行关闭
+                Objects.requireNonNull( writer ).close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
