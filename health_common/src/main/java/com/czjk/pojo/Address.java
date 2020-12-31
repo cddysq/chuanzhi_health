57753d320d0cb4e70817dfb48dd3b9404311ed1d
Rainbow Cat <2056423011@qq.com>
2020-12-31 12:03:51 +0800

    mp 引入

--- health_common/src/main/java/com/czjk/pojo/Address.java
+++ health_common/src/main/java/com/czjk/pojo/Address.java
@@ -1,5 +1,8 @@
 package com.czjk.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,19 +15,21 @@ import java.math.BigDecimal;
  * 地址管理
  *
  * @author Haotian
- * @version 1.0.0
+ * @version 2.0
  * @date 2020/5/23 15:39
  **/
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@TableName("t_address")
 public class Address implements Serializable {
     private static final long serialVersionUID = -5243767030375722029L;
 
     /**
      * 主键
      */
+    @TableId(type = IdType.AUTO)
     private Integer id;
 
     /**
---
+++ health_service_provider/src/main/java/com/czjk/config/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package com.czjk.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mp 配置类
+ *
+ * @author Haotian
+ * @version 2.0
+ * @date 2020/12/29 11:15
+ */
+@Configuration
+@MapperScan("com.czjk.dao")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor( DbType.MYSQL));
+        return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
+    }
+
+}
